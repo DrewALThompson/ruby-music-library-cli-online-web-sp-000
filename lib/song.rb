b1dfdb83ca71567c75c12bcf308707e6848ce6a6
Drew Thompson <drewallynthompson@gmail.com>
2020-09-12 02:23:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,8 @@
     song_name = filename.split(" - ")[1].split(".mp3")[0]
     artist_name = filename.split(" - ")[0]
     genre_name = filename.split(" - ")[1].split(".mp3")[1]
-    
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
   end
 end 
