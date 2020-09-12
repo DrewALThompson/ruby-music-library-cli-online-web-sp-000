a2a78d8dd0f736847e75cc97c7d2bb3e7a712d86
Drew Thompson <drewallynthompson@gmail.com>
2020-09-12 01:52:05 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+ class Song < Base
+  attr_accessor :name, :artist, :genre
+  extend Concerns::Findable
+  
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+    self.artist=(artist) if artist != nil
+    self.genre=(genre) if genre != nil
+  end 
+  
+  def self.create(song)
+    new_song = self.new(song)
+    new_song.save
+    new_song
+  end
+  
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    genre.songs.include?(self) ? nil : genre.songs << self
+  end
+end 
